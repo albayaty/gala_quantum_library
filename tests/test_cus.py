4615c59cc91a2e9e67cec4e624a655c3ca0c103e
Ali Al-Bayaty <albayaty@users.noreply.github.com>
2024-08-15 17:23:48 -0700

    Update test_cus.py

--- tests/test_cus.py
+++ tests/test_cus.py
@@ -1,8 +1,11 @@
-from grover_controlled_diffuser import *
+from gala_quantum_library import *
 import pytest
 
-def test_cus():
-	qubits = [3, 4, 5, 6, 7]
+def test_gala():
+	qubits = [3, 4, 5]
 	
-	for qubit in qubits:
-		assert num_CUs_gates(qubit) == (qubit*4) + 1
+	for n in qubits:
+		assert num_gates_GALA_Boolean(n) == 2 + 2**(n-1) + (2**(n-1))-1		
+		assert num_gates_GALA_Fredkin(n) == 2 + 2**(n-1) + (2**(n-1))+1
+		assert num_gates_GALA_Miller(n) == 2 + 2**(n-1) + (2**(n-1))-1+((n-1)*2)
+		assert num_gates_GALA_CSX(n) == 4 + (2**(n-1))+2 + (2**(n-1))-1
